a05ae9a4465764b4fbd77708d44572dda658d88f
GabrielLeme <bielleme2012@hotmail.com>
2016-10-12 13:20:53 -0300

    Initial commit

---
+++ ProjectEuler-LargestPalindromeProduct/ProjectEuler-LargestPalindromeProduct/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler_LargestPalindromeProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int product, largestPalindrome = 0;
+
+            for (int x = 100; x < 1000; x++)
+            {
+                for (int y = 100; y < 1000; y++)
+                {
+                    product = x * y;
+                    
+                    if (isPalindrome(product))
+                    {
+                        if (product > largestPalindrome)
+                            largestPalindrome = product;
+                        Console.WriteLine(x + " * " + y + " = " + product);
+                    }
+                }
+            }
+            Console.WriteLine("Largest palindrome: " + largestPalindrome);
+            Console.ReadKey();
+        }
+
+        static bool isPalindrome(int n)
+        {
+            char[] chars = n.ToString().ToCharArray();
+            Array.Reverse(chars);
+            string reverseString = new string(chars);
+
+            if (n.ToString() == reverseString)
+                return true;
+            return false;
+        }
+    }
+}
